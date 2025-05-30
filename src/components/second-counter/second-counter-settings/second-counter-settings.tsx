4a22f8a02a3efa486f570802c5bbfeb815baf1c4
nedonebo21 <cursed.void10007@gmail.com>
2025-05-30 12:38:03 +0400

    add checkValues func, refactor handleValueChange(counter2)

--- src/components/second-counter/second-counter-settings/second-counter-settings.tsx
+++ src/components/second-counter/second-counter-settings/second-counter-settings.tsx
@@ -1,25 +1,27 @@
 import s from '../second-counter.module.css'
 import {Input} from "../../../shared/ui/input/input.tsx";
 import {Button} from "../../../shared/ui/button/button.tsx";
-import type {StatusType} from "../second-counter.tsx";
+import type {InputErrorsType, StatusType} from "../second-counter.tsx";
 import {useState} from "react";
 
 type Props = {
     updateCounter: (min: number, max: number) => void
-    setError: (error: string | null) => void
     setStatus: (status: StatusType) => void
-    error: string | null
     setIsChanged: (isChanged: boolean) => void
     minValue: number
     maxValue: number
+    inputError: InputErrorsType
+    setInputError: (inputError: InputErrorsType) => void
 }
+
 
 export const SecondCounterSettings = (props: Props) => {
     const {
         updateCounter,
-        setError,
         setStatus,
         setIsChanged,
+        setInputError,
+        inputError,
         minValue,
         maxValue
     } = props
@@ -28,46 +30,68 @@ export const SecondCounterSettings = (props: Props) => {
         min: minValue,
         max: maxValue
     })
-    const [inputError, setInputError] = useState<{min?: string, max?: string}>({})
+
 
     const {min, max} = valuesSettings
+
+    const checkValues = (min?: number, max?: number) => {
+        const errors:InputErrorsType = {}
+        if (min === undefined || isNaN(min)){
+            errors.min = 'incorrect value'
+        } else if (min <= -1){
+            errors.min = 'min value cannot be negative'
+        }
+
+        if(max === undefined || isNaN(max)){
+            errors.max = 'incorrect value'
+        } else if (max <= 0){
+            errors.max = 'max value must be > 0'
+        }
+
+        if (max !== undefined && min !== undefined && !isNaN(max) && !isNaN(min)){
+            if (min >= max) {
+                if (!errors.min){
+                    errors.min = 'min value must be less than max'
+                }
+                if (!errors.max) {
+                    errors.max = 'max value must be greater than min'
+                }
+            }
+        }
+
+        return errors
+    }
+
     const handleValueChange = (value: number, type: 'min' | 'max') => {
+
+        let lockMinInputValue = value
+        if (type === 'min' && value < - 1){
+            lockMinInputValue = -1
+        }
+        if (type === 'max' && value < 0){
+            lockMinInputValue = 0
+        }
+
         const inputValues = {
             ...valuesSettings,
-            [type]: value
+            [type]: lockMinInputValue
         }
-        const tempError: {min?: string, max?: string} = {}
-        if (value <= -1) {
-            setError(`${type} value cannot be negative`)
-            tempError[type] = `${type} value cannot be negative`
-            setInputError(tempError)
-            if (value === -1) {
-                setValuesSettings(inputValues)
-            }
-            return
-        }
-        if (inputValues.min >= inputValues.max ||  isNaN(inputValues.min) || isNaN(inputValues.max)) {
-            setError('max value must be greater than min')
-            tempError[type] = `${type} value cannot be negative`
-            setInputError(tempError)
-        } else {
-            setError(null)
-        }
-        setInputError(tempError)
         setValuesSettings(inputValues)
+
+        const errors = checkValues(inputValues.min, inputValues.max)
+        setInputError({...errors, lastChanged: type})
+
+        const valuesUnchanged = inputValues.min === minValue && inputValues.max === maxValue
+        setIsChanged(!valuesUnchanged)
     }
     const handleSave = () => {
         if (min >= max || min < 0 || max < 0) return
         updateCounter(min, max)
         setStatus('counting')
+        setIsChanged(false)
     }
-    const valuesUnchanged = min === minValue && max === maxValue
-    const isSaveDisabled =
-        valuesSettings.min < 0 || valuesSettings.max < 0 ||
-        valuesSettings.min >= valuesSettings.max ||
-        (valuesSettings.min === minValue && valuesSettings.max === maxValue)
 
-    setIsChanged(!valuesUnchanged)
+    const isSaveDisabled = min < 0 || max < 0 || min >= max || (min === minValue && max === maxValue)
 
     return (
         <div className={s.counterSettings}>
