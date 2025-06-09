9c5e90a1e39b42936acc9947053d7a4ee60f8dbb
nedonebo21 <cursed.void10007@gmail.com>
2025-06-09 18:21:44 +0400

    remove second-counter

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,11 +1,9 @@
 import {Counter} from "../components/main-counter/counter.tsx";
-// import {SecondCounter} from "../components/second-counter/second-counter.tsx";
 
 export const HomePage = () => {
     return (
         <div className={"wrapper"}>
             <Counter min={2} max={5}/>
-            {/*<SecondCounter/>*/}
         </div>
     )
 }
