54aa8dbf53f2e0fe9f4573349f0f1fd3786beb9a
nedonebo21 <cursed.void10007@gmail.com>
2025-07-26 12:24:51 +0400

    add localStorage

--- src/app/store.ts
+++ src/app/store.ts
@@ -4,8 +4,19 @@ import {CounterReducer} from "../features/model/counter-reducer.ts";
 const rootReducer = combineReducers({
   counter: CounterReducer
 })
+
+
+
+const presistedCounterToString = localStorage.getItem('state')
+const preloadedState = presistedCounterToString ? JSON.parse(presistedCounterToString) : undefined
+
 export const store = configureStore({
   reducer: rootReducer,
+  preloadedState
+})
+
+store.subscribe(() => {
+  localStorage.setItem('state', JSON.stringify(store.getState()))
 })
 
 export type RootState =ReturnType<typeof store.getState>
